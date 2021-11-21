3ee619755517066895def128658d058b5e5842be
wanggang <arthurwanggang@yeah.net>
2021-11-21 22:32:12 +0800

    x

---
+++ src/main/java/com/example/algorithm/AlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmApplication.class, args);
+    }
+
+}
