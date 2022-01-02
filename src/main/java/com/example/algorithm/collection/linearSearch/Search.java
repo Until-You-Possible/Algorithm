1603391e9779a9033e60c30a0cdf318b91093790
wanggang <arthurwanggang@yeah.net>
2022-01-02 14:26:27 +0800

    x

--- src/main/java/com/example/algorithm/collection/linearSearch/Search.java
+++ src/main/java/com/example/algorithm/collection/linearSearch/Search.java
@@ -10,6 +10,15 @@ public class Search {
             }
         }
         return -1;
+
+        // 关于复杂度的分析
+        // 通常看最差的的预估，而不是最理想
+        // 也可以说 算法运行的上界
+        // 数据规模 当前例 就是  n = data.length
+        // 上界 T = n;
+        // O(n) 常量O不重要
+        // 如果是O(n)复杂度 表示 随着n的量增大 复杂度呈线性关系
+
     }
 
     public static void main(String[] args) {
@@ -21,6 +30,8 @@ public class Search {
         Team teamMember = new Team("wow");
         int res1 = Search.linearSearch(teams, teamMember);
         System.out.println("res00000  " + res1);
+
+
     }
 
 }
